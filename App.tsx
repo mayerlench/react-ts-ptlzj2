1f9569cc0aed6d725c7963865ba1ba3c71962fc7
mayerlench <18517345+mayerlench@users.noreply.github.com>
2022-09-07 12:59:27 -0400

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import flatten from 'ramda/src/flatten';
 import * as React from 'react';
 import { useTimer } from 'use-timer';
 import './style.css';
@@ -10,28 +11,33 @@ const yellow =
   'https://github.com/mayerlench/react-ts-ptlzj2/blob/main/images/7ff90ef6-87ca-477c-963c-8d5b8dcc6d73.jpg?raw=true';
 
 type ThreatLevel = {
-  threatLevel: string;
+  image: string;
+  level: string;
   duration: number;
 };
 
-const intitalCountdown = 5;
-
 export default function App() {
-  const [threatLevel, setThreatLevel] = React.useState<ThreatLevel>(null);
-  const [threatLevels, setThreatLevels] = React.useState<ThreatLevel[]>(null);
+  const [threat, setThreat] = React.useState<ThreatLevel>(null);
+  const [threats, setThreats] = React.useState<ThreatLevel[]>(null);
+  const [assessTime, setAssessTime] = React.useState(5);
 
-  const [gameCompleted, setGameCompleted] = React.useState(false);
+  const [delayedStart, setDelayedStart] = React.useState(5);
+
+  const [gameStatus, setGameStatus] = React.useState<
+    'countdown' | 'started' | 'completed' | null
+  >(null);
+
   const { start, advanceTime, time } = useTimer({
     initialTime: 1,
     endTime: 0,
     timerType: 'DECREMENTAL',
     onTimeOver: () => {
-      setNextThreatLevel(threatLevels);
+      setNextThreatLevel(threats);
     },
   });
 
   const countdown = useTimer({
-    initialTime: intitalCountdown,
+    initialTime: delayedStart,
     endTime: 0,
     timerType: 'DECREMENTAL',
     onTimeOver: () => {
@@ -44,53 +50,56 @@ export default function App() {
   }, [time]);
 
   const startCountdown = () => {
+    setGameStatus('countdown');
     countdown.start();
+    countdown.advanceTime(countdown.time + -delayedStart);
   };
 
-  const setNextThreatLevel = (threatLevels: ThreatLevel[]) => {
-    const next = threatLevels?.[0];
+  const setNextThreatLevel = (threats: ThreatLevel[]) => {
+    const next = threats?.[0];
     console.log(next);
     if (!next) {
       countdown.reset();
-      return setGameCompleted(true);
+      return setGameStatus('completed');
     }
-    setThreatLevel(next);
+    setThreat(next);
 
-    setThreatLevels(threatLevels.slice(1));
+    setThreats(threats.slice(1));
     start();
     advanceTime(-next.duration + 1);
   };
 
   const startGame = () => {
-    setGameCompleted(false);
+    setGameStatus('started');
     const game = buildGame();
-    setThreatLevels(game);
-    console.log('GAME', game);
+    setThreats(game);
     setNextThreatLevel(game);
   };
 
   const stopGame = () => {
-    setThreatLevel(null);
-    setGameCompleted(false);
+    setThreat(null);
+    setGameStatus(null);
   };
 
   const buildGame = () => {
-    const initalThreatTime = randomIntFromInterval(5, 10);
-    const initalAssessTime = randomIntFromInterval(5, 8);
+    const getAssessLevel = () => ({
+      image: yellow,
+      duration: assessTime,
+      level: 'assess',
+    });
+    const getFireLevel = (duration: number) => ({
+      image: red,
+      duration: assessTime,
+      level: 'fire',
+    });
 
-    const threatRengage = Boolean(randomIntFromInterval(0, 20) > 10);
-    const threatRengageTime = randomIntFromInterval(3, 9);
-
-    const game = [
-      { threatLevel: red, duration: initalThreatTime },
-      { threatLevel: yellow, duration: initalAssessTime },
-    ].concat(
-      threatRengage
-        ? [
-            { threatLevel: red, duration: threatRengageTime },
-            { threatLevel: yellow, duration: 4 },
-          ]
-        : []
+    const game = flatten(
+      [...Array(randomIntFromInterval(1, 3)).keys()].map((m) => {
+        return [
+          { ...getFireLevel(randomIntFromInterval(5, 10)) },
+          { ...getAssessLevel() },
+        ];
+      })
     );
 
     return game;
@@ -105,29 +114,47 @@ export default function App() {
     <div className="container text-center">
       <h1 className="mt-5">Threat assessment practice</h1>
       <div className="mt-5">
-        {!gameCompleted && (
+        {gameStatus === 'started' && (
           <img
-            src={threatLevel?.threatLevel}
+            src={threat?.image}
             style={{ maxHeight: 750 }}
             className={
-              threatLevel?.threatLevel === red
+              threat?.image === red
                 ? 'img-fluid animate__animated animate__pulse animate__infinite animate__faster'
                 : 'img-fluid'
             }
           />
         )}
-        {gameCompleted && (
+        {gameStatus === 'completed' && (
           <img src={green} style={{ maxHeight: 750 }} className="img-fluid" />
         )}
       </div>
 
       <div>
-        {countdown.time === intitalCountdown && (
-          <button onClick={startCountdown} className="btn btn-primary">
-            Start
-          </button>
+        {(!gameStatus || gameStatus === 'completed') && (
+          <div>
+            <div style={{ width: 100 }} className="m-auto">
+              <input
+                placeholder="Delay"
+                className="form-control"
+                value={delayedStart}
+                onChange={(e) => {
+                  const value = Number(e.target.value);
+                  if (isNaN(value) || value < 0) return;
+
+                  setDelayedStart(Number(e.target.value));
+                }}
+              />
+            </div>
+            <button
+              onClick={startCountdown}
+              className="btn btn-primary btn-lg mt-2"
+            >
+              Start
+            </button>
+          </div>
         )}
-        {countdown.time !== 0 && (
+        {gameStatus === 'countdown' && (
           <label className="m-3">{countdown.time} second delayed start</label>
         )}
       </div>
